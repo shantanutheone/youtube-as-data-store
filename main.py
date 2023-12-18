15618354c7c70ed61734440a3be832f16f5c8a1b
Shantanu Tripathi <shaantanutripathi@gmail.com>
2023-12-18 19:22:25 +0530

    full workflow completed

--- main.py
+++ main.py
@@ -10,20 +10,17 @@ binary_string = convert_file_to_binary(file_path)
 # Make Image with Binary
 binary_to_images(binary_string, "data/initial_images/")
 
-# Test by making back csv with images - Done
-# binary_res = images_to_binary("data/initial_images")
-# print(binary_res == binary_string)
-# binary_to_zip_file(binary_res, "data/zipped_files/5mb_back.zip")
-# unzip("data/zipped_files/5mb_back.zip", "extracted_files")
-
 # Make Video with Binary Images
 images_to_video("data/initial_images/", "videos/output_video.avi")
 
 # Extract Image back from Video
+video_to_images("videos/output_video.avi", "data/extracted_images")
 
 # Extract Binary from Concatenated Image 
+binary_res = images_to_binary("data/extracted_images/")
 
 # Make Zip file with binary
+binary_to_zip_file(binary_res, "data/zipped_files/5mb_back.zip")
 
 # Extract Original File
-
+unzip("data/zipped_files/5mb_back.zip", "extracted_files")
